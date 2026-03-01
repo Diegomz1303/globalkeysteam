bde5d648710671c1e1075359e9093c4781ee2ca6
Diegomz1303 <diegoamz123@gmail.com>
2026-02-28 20:11:58 -0500

    actualizacioin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // <--- Esta es la única línea nueva añadida para solucionar tu error
     remotePatterns: [
       {
         protocol: 'https',
